deaca541f95aa91cc121a179317976697e169411
Anyanwu Stephanie <stephanieanyanwu605@gmail.com>
2023-09-12 14:48:00 -0700

    z

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,11 +7,13 @@
 int main(void)
 {
 	int i = 0;
-	long j =1, k=2;
+	long j = 1, k = 2;
 
 	while (i < 50)
 	{
-		if (i ==0)
+		if (i == 0)
+			printf(", %ld", k);
+		else if (i == 1)
 			printf(", %ld", k);
 		else
 		{
