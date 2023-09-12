9d583540062ca878f158b6b3b4398e49b5208014
Anyanwu Stephanie <stephanieanyanwu605@gmail.com>
2023-09-12 14:19:16 -0700

    g

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (num =0; num <= n; num++)
+		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
 			for (mult = 1; mult <= n; mult++)
@@ -22,7 +22,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
-				if (prod <= 100)
+				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
 					_putchar(((prod /10)) % 10 + '0');
