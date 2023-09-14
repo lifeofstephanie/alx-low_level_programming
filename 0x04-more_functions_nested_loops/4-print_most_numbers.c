a6c6dfad7ecd39a6a78e6eb9749abb31e71fadeb
Anyanwu Stephanie <stephanieanyanwu605@gmail.com>
2023-09-14 05:04:38 -0700

    ll

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,16 +3,22 @@
 /**
  * print_most_numbers - print num except 2 and 4
  *
- * Return: void
+ * Return: numbers from 0 to 9
  */
 void print_most_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if(|(c == '2' || c == '4'))
-			_putchar(c);
+		if (c == '2' || c == '4')
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c + '0');
+		}
 	}
 	_putchar('\n');
 }
