d4711568abed9c2ec67f2252ca7917e2b58e8baa
Anyanwu Stephanie <stephanieanyanwu605@gmail.com>
2023-10-02 12:53:07 -0700

    sorry

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#include <string.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,3 +22,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+
+#endif
